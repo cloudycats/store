5a028932ae69528bb30559fbd140f855ce31dc12
catinsky <wangchangat@163.com>
2025-10-25 13:07:03 +0800

    Update drive.js

--- drive.js
+++ drive.js
@@ -9,7 +9,7 @@
     let obj = JSON.parse(body);
     if (obj && obj.data) {
       obj.data.vipType = 1;
-      obj.data.vipExpireTime = "2027-12-31T23:59:59.000+00:00";
+      obj.data.vipExpireTime = "林北终身会员!!";
     }
     $done({ body: JSON.stringify(obj) });
   } catch (e) {
