dd5572af623b57be543e3ed3d55c079990a5a559
catinsky <wangchangat@163.com>
2026-02-11 14:50:56 +0800

    Update car_fix.js

--- car_fix.js
+++ car_fix.js
@@ -3,7 +3,7 @@ let body = $response.body;
 try {
   let obj = JSON.parse(body);
   if (obj && obj.data && obj.data.user) {
-    obj.data.user.vip = 1;
+    obj.data.user.vip = 3;
     // 可选：也可设置 vip_start_date/vip_end_date
     obj.data.user.vip_start_date = "2026-02-11 00:00:00";
     obj.data.user.vip_end_date = "2099-12-31 23:59:59";
