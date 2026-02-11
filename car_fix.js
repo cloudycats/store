5cc42d55e85b65a57574c6c0d096cd1ccd39b180
catinsky <wangchangat@163.com>
2026-02-11 14:48:15 +0800

    Update user VIP status in car_fix.js
    Modify user VIP status and set VIP dates in response.

---
+++ car_fix.js
@@ -0,0 +1,13 @@
+// replace_vip.js
+let body = $response.body;
+try {
+  let obj = JSON.parse(body);
+  if (obj && obj.data && obj.data.user) {
+    obj.data.user.vip = 1;
+    // 可选：也可设置 vip_start_date/vip_end_date
+    obj.data.user.vip_start_date = "2026-02-11 00:00:00";
+    obj.data.user.vip_end_date = "2099-12-31 23:59:59";
+    body = JSON.stringify(obj);
+  }
+} catch (e) {}
+$done({body});
